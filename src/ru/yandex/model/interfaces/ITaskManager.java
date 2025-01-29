137ed3cd0b3978a7e1a0eee1122af2b371819efd
I1nur <tuxtarov0475@gmail.com>
2025-01-29 20:30:30 +0400

    checkstyle: add whitespace in interface

--- src/ru/yandex/model/interfaces/ITaskManager.java
+++ src/ru/yandex/model/interfaces/ITaskManager.java
@@ -9,20 +9,24 @@ import java.util.List;
 
 public interface ITaskManager {
     Task getTaskById(Integer id);
+
     Task removeById(Integer id);
+
     Task createTask(Task task);
+
     Task updateTask(Task task);
+
     ArrayList<Subtask> getListSubTasks();
+
     ArrayList<Epic> getListEpics();
+
     ArrayList<Task> getListTasks();
 
     List<Task> getHistory();
+
     void deleteTasks();
+
     void deleteSubtasks();
+
     void deleteEpics();
 }
-//    a. Менеджер сам не выбирает статус для задачи. Информация о нём приходит менеджеру вместе с информацией о самой задаче. По этим данным в одних случаях он будет сохранять статус, в других будет рассчитывать.
-//        b. Для эпиков:
-//        если у эпика нет подзадач или все они имеют статус NEW, то статус должен быть NEW.
-//        если все подзадачи имеют статус DONE, то и эпик считается завершённым — со статусом DONE.
-//        во всех остальных случаях статус должен быть IN_PROGRESS.
